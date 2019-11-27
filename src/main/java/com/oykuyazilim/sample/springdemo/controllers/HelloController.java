8dddb22c264743d1907d6cd00045cd7439e418b7
Nurettin Onur TUĞCU <onur.tugcu@whipdata.com>
2019-11-27 19:50:36 +0300

    hello controller calls hell oservice

--- src/main/java/com/oykuyazilim/sample/springdemo/controllers/HelloController.java
+++ src/main/java/com/oykuyazilim/sample/springdemo/controllers/HelloController.java
@@ -2,15 +2,10 @@ package com.oykuyazilim.sample.springdemo.controllers;
 
 import com.oykuyazilim.sample.springdemo.services.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
 
 @RestController()
 @RequestMapping("/hello")
